602a1da5dd0dd4c3073915787adf8570da29629d
SanchesVitor <vitorsanches06@hotmail.com>
2023-10-03 18:46:25 -0300

    Init styles for mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-4xl">
+    <View className="bg-gray-900 flex-1 items-center justify-center">
+      <Text className="text-zinc-50 font-bold text-4xl font-title">
         ESSAS PAPAGAIDA AE!
       </Text>
       <StatusBar style="light" translucent />
