1d0a07ebe76b6305739aeb635c5296e872479f34
SanchesVitor <vitorsanches06@hotmail.com>
2023-10-28 12:35:27 -0300

    Create background blur

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,34 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-
+import { ImageBackground } from 'react-native';
 
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
+} from '@expo-google-fonts/roboto';
 
-import {
-  BaiJamjuree_700Bold,
-} from '@expo-google-fonts/bai-jamjuree'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+
+import blurBg from './src/assets/bg-blur.png';
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
-  })
-  
+  });
+
   if (!hasLoadedFonts) {
     return null;
   }
 
   return (
-    <View className="bg-gray-900 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 text-4xl font-bold font-alt">
-        essas papagaiada AE
-      </Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   );
 }
