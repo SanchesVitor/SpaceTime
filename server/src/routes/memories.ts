a05d0be1d52191713695be6f3a4db8011deb2aea
SanchesVitor <vitorsanches06@hotmail.com>
2023-11-02 15:34:08 -0300

    Created CRUD for DELETE

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -1,24 +1,74 @@
-import { FastifyInstance } from "fastify";
-import { prisma } from "../lib/prisma";
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { prisma } from '../lib/prisma';
 
 export async function memoriesRoutes(app: FastifyInstance) {
   app.get('/memories', async () => {
-  
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    });
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat('...'),
+      };
+    });
   });
 
-  app.get('/memories/:id', async () => {
-  
+  app.get('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    });
+
+    return memory;
   });
 
-  app.post('/memories', async () => {
-  
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    });
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body);
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: '7b824ffc-e9d9-4616-8eae-47cb60a0df56',
+      },
+    });
+
+    return memory;
   });
 
-  app.put('/memories/:id', async () => {
-  
-  });
+  app.put('/memories/:id', async () => {});
 
-  app.delete('/memories/:id', async () => {
-  
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    });
   });
 }
